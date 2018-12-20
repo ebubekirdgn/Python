5969c9896409293b0ee0960f4cba81e5abc70354
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2018-12-20 19:13:44 +0300

    Add files via upload

---
+++ baglaclari-listeden-silme.py
@@ -0,0 +1,13 @@
+﻿
+baglaclar=['açıkçası','ama','ancak','bile','oysa','oysaki','öyleyse','üstelik','ve','veya','veyahut','yahut','yalnız','hatta','yine','yoksa','zira','ile','hâlbuki']
+kelimeler = ['ve','veya','Karpuz','Karpuz','Karpuz','Elma','hatta','ile','hâlbuki']
+ 
+ 
+def func(words, conjunctions):
+    res = []
+    for word in words:
+        if word not in conjunctions:
+            res.append(word)
+    return res
+
+print(func(kelimeler, baglaclar))
